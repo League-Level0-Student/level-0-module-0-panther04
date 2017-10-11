307fb16b06acee163c720d2a786eab9956374c13
Ganymede007 <moyhuro@gmail.com>
2017-10-10 19:23:39 -0700

    RobotSquare

--- src/RobotSquare.java
+++ src/RobotSquare.java
@@ -11,22 +11,23 @@ public class RobotSquare {
     public static void main(String[] args) throws Exception {
  
     	// 1. Make a new Robot
-
+Robot andres= new Robot();
 
         // 3. Put the robot's pen down
-
+andres.penDown();
 
         // 6. Make the robot move as fast as possible
-
+andres.setSpeed(10);
 
         // 5. Do everything below here 4 times
+for (int i = 0; i <4  ; i++) {
 
 
         //         2. Move your robot 200 pixels
-
+andres.move(200);
 
         //         4. Turn the robot 90 degrees to the right (90 degrees)
-
-
+andres.turn(90);
+}
     }
 }
